572e43c745cd7781ad8dc6e8139e6ec337f590e3
lucascabraldeve <lucas_cabral.deve@outlook.com>
2020-12-19 15:26:36 -0300

    Animação para a splash screen adicionada.

--- app/src/main/java/com/lucascabral/hub4all/activities/SplashActivity.kt
+++ app/src/main/java/com/lucascabral/hub4all/activities/SplashActivity.kt
@@ -4,20 +4,30 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.view.WindowManager
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import com.lucascabral.hub4all.R
+import kotlinx.android.synthetic.main.activity_splash.*
 import okhttp3.internal.http2.Http2Reader
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
         setContentView(R.layout.activity_splash)
-
         supportActionBar?.hide()
+
+        val topAnimation: Animation = AnimationUtils.loadAnimation(this, R.anim.top_animation)
+        val bottomAnimation: Animation = AnimationUtils.loadAnimation(this, R.anim.bottom_animation)
+
+        splashLogoImageView.animation = topAnimation
+        splashTitleTextView.animation = bottomAnimation
 
         Handler().postDelayed({
             val intent = Intent(applicationContext, LoginActivity::class.java)
             startActivity(intent)
             finish()
-        }, 3000)
+        }, 3500)
     }
 }
